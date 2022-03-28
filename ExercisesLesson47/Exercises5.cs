890b4c00914d248a345ec973ca80206b0a81e04c
Trieu Than <trieuthan92pro@gmail.com>
2022-03-28 23:38:21 +0700

    Add update exercises lesson 4.7

---
+++ ExercisesLesson47/Exercises5.cs
@@ -0,0 +1,36 @@
+﻿/// <summary>
+/// @author Branium Academy
+/// @see https://braniumacademy.net
+/// @version 2022.03.28
+/// </summary>
+
+using System;
+
+namespace ExercisesLesson47
+{
+    class Exercises5
+    {
+        static void Main()
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int test = 1; test <= t; test++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Test {test}: {CountDigits(n)}");
+            }
+        }
+
+        // phương thức đệ quy đếm số lượng các chữ số của n
+        static int CountDigits(int n)
+        {
+            if (n < 10)
+            {
+                return 1;
+            }
+            else
+            {
+                return 1 + CountDigits(n / 10);
+            }
+        }
+    }
+}
---
+++ ExercisesLesson47/Exercises7.cs
@@ -0,0 +1,39 @@
+﻿/// <summary>
+/// @author Branium Academy
+/// @see https://braniumacademy.net
+/// @version 2022.03.28
+/// </summary>
+
+using System;
+
+namespace ExercisesLesson47
+{
+    class Exercises7
+    {
+        static void Main()
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int test = 1; test <= t; test++)
+            {
+                string s = Console.ReadLine();
+                Console.Write($"Test {test}: ");
+                PrintCharacter(s, s.Length - 1);
+                Console.WriteLine();
+            }
+        }
+
+        // phương thức đệ quy in ra các chữ cái của chuỗi kí tự theo thứ tự đảo ngược
+        static void PrintCharacter(string str, int n)
+        {
+            if (n < 0)
+            {
+                return;
+            }
+            else
+            {
+                Console.Write(str[n]);
+                PrintCharacter(str, n - 1);
+            }
+        }
+    }
+}
