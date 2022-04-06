20d5ff9b291b9401dbb38b85b8488227dc508851
Trieu Than <trieuthan92pro@gmail.com>
2022-04-06 15:49:50 +0700

    Add ex4+5

---
+++ ExercisesLesson61/Exercises4.cs
@@ -0,0 +1,80 @@
+﻿///<resultmary>
+///<author>Branium Academy</author>
+///<see cref="Trang chu" href="https://braniumacademy.net"/>
+///<version>2022.04.06</version>
+///</resultmary>
+
+using System;
+
+namespace ExercisesLesson61
+{
+    class BankAccount
+    {
+        public string AccountNumber { get; set; } // số tài khoản
+        public string Owner { get; set; } // chủ tài khoản
+        public long Balance { get; set; } // số dư
+        public string Bank { get; set; } // ngân hàng phát hành
+        public string ExpiredDate { get; set; } // ngày hết hạn
+        public int PIN { get; set; } // mã pin
+
+        public BankAccount()
+        {
+
+        }
+
+        public BankAccount(string accNum, string owner, 
+            long balance, string bank, string exp, int pin)
+        {
+            AccountNumber = accNum;
+            Owner = owner;
+            Balance = balance;
+            Bank = bank;
+            ExpiredDate = exp;
+            PIN = pin;
+        }
+
+        public void CheckBalance()
+        {
+            Console.WriteLine($"==> Thong tin so du tai khoan {AccountNumber}: ");
+            Console.WriteLine("So tien hien co trong tai khoan la: " + Balance + "d");
+        }
+        // rút tiền 
+        public long Withdraw(long amount)
+        {
+            if (amount < Balance)
+            {
+                Balance -= amount;
+                return amount;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        // nạp tiền vào tài khoản
+        public long Deposit(long amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                return amount;
+            }
+            return 0;
+        }
+        // chuyển tiền đến ngân hàng khác
+        public long Transfer(BankAccount other, long amount)
+        {
+            if (other != null && amount < Balance)
+            {
+                other.Balance += amount;
+                Balance -= amount;
+                return amount;
+            }
+            return 0;
+        }
+    }
+    class Exercises4
+    {
+
+    }
+}
---
+++ ExercisesLesson61/Exercises5.cs
@@ -0,0 +1,41 @@
+﻿///<resultmary>
+///<author>Branium Academy</author>
+///<see cref="Trang chu" href="https://braniumacademy.net"/>
+///<version>2022.04.06</version>
+///</resultmary>
+
+using System;
+
+namespace ExercisesLesson61
+{
+    class Student
+    {
+        public string Id { get; set; } // mã sinh viên
+        public string FirstName { get; set; } // tên
+        public string LastName { get; set; } // họ
+        public string MidName { get; set; } // đệm
+        public string Address { get; set; } // địa chỉ
+        public float Gpa { get; set; } // điểm TB
+        public string Major { get; set; } // chuyên ngành
+
+        public Student()
+        {
+
+        }
+
+        public Student(string id, string first, string last,
+            string mid, string address, float gpa, string major)
+        {
+            Id = id;
+            FirstName = first;
+            LastName = last;
+            MidName = mid;
+            Gpa = gpa;
+            Address = address;
+            Major = major;
+        }
+    }
+    class Exercises5
+    {
+    }
+}
