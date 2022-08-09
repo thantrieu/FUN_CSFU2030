8452bb5221c91e404f80d009b62a5d036394a1be
Trieu Than <trieuthan92pro@gmail.com>
2022-08-09 09:50:54 +0700

    Add comments

--- ExercisesLesson32/Exercises3.cs
+++ ExercisesLesson32/Exercises3.cs
@@ -6,17 +6,18 @@ namespace ExercisesLesson32
     {
         static void Main()
         {
-            var data = Console.ReadLine().Split(' ');
-            int a = int.Parse(data[0]);
-            int b = int.Parse(data[1]);
-            if (a == b)
+            // nhập hai số liên tiếp. Ví dụ nhập 2 3 rồi nhấn enter.
+            var data = Console.ReadLine().Split(' ');   // tách hai số tại vị trí dấu cách thành số "2" và số "3" ở dạng string
+            int a = int.Parse(data[0]);                 // chuyển đổi string số thứ nhất thành số kiểu nguyên. Vd "2" -> 3
+            int b = int.Parse(data[1]);                 // chuyển đổi string số thứ hai thành số kiểu nguyên. Vd "3" -> 3
+            if (a == b)                                 // nếu hai số bằng nhau
             {
-                Console.WriteLine("EQUAL");
+                Console.WriteLine("EQUAL");             // hiển thị EQUAL ra màn hình
             }
-            else
+            else                                        // ngược lại, hai số khác nhau
             {
-                var diff  = Math.Abs(a - b);
-                Console.WriteLine($"DIFFERENT {diff}");
+                var diff  = Math.Abs(a - b);            // biến diff lưu giá trị lệch nhau giữa hai số
+                Console.WriteLine($"DIFFERENT {diff}"); // hiển thị độ lệch của hai số trong biến diff ra màn hình
             }
         }
     }
--- ExercisesLesson32/Exercises4.cs
+++ ExercisesLesson32/Exercises4.cs
@@ -6,13 +6,16 @@ namespace ExercisesLesson32
     {
         static void Main()
         {
-            // nhập dữ liệu vào từ bàn phím và tách các số tại vị trí có dấu cách
-            var data = Console.ReadLine().Split(' ');
+            // nhập 3 số liên tiếp cách nhau bởi dấu cách vào từ bàn phím và tách các số tại vị trí có dấu cách
+            // ví dụ gõ: 100 200 300 sau đó nhấn enter
+            // do hàm đọc dữ liệu vào từ bàn phím trả về giá trị string "100 200 300"
+            // nên ta dùng hàm Split(' ') để tách các thành phần trong string tại vị trí có dấu cách
+            var data = Console.ReadLine().Split(' '); // tách thành 3 string "100", "200", "300"
             int a = int.Parse(data[0]); // chuyển string sang int
             int b = int.Parse(data[1]);
             int c = int.Parse(data[2]);
             
-            // hiển thị kết quả
+            // so sánh và hiển thị kết quả
             if(a == b && b == c)
             {
                 Console.WriteLine("NO RESULT");
