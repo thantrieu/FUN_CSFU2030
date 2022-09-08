983548087e40bdef7825cce55501fbc6c9a33bd5
Trieu Than <trieuthan92pro@gmail.com>
2022-09-08 16:03:10 +0700

    Add update exercises lesson 3.7.11

--- ExercisesLesson37/Exercises11.cs
+++ ExercisesLesson37/Exercises11.cs
@@ -6,14 +6,25 @@ namespace ExercisesLesson37
     {
         static void Main()
         {
+            // ta sẽ vẽ các đường viền tại vị trí 2 cạnh là 1 hoặc n và nửa trên của hai đường chéo.
+            // nếu n lẻ thì ta làm tròn n/2 lên số nguyên gần nhất để lấy mũi phong bì
+            // nếu n chẵn thì ta không vẽ mũi phong bì mà để nó dạng tù, không nhọn.
+
             int n = int.Parse(Console.ReadLine());
+            int middle = (int)Math.Round(n / 2.0);
+            if (n % 2 != 0)
+            {
+                // n lẻ -> làm tròn lên 1 nửa của n để lấy giá trị tròn số. ví dụ n = 11 -> vẽ đến hàng số 6 từ trên xuống
+                middle = (int)Math.Round((n + 1) / 2.0);
+            }
             for (int i = 1; i <= n; i++)
             {
                 for (int j = 1; j <= n; j++)
                 {
                     if (i == 1 || i == n || j == 1 || j == n ||
-                        ((i == j) && i <= (int)Math.Round(n / 2.0)) ||
-                        (i + j == n + 1 && i <= (int)Math.Round(n / 2.0)))
+                        ((i == j) && i <= middle) ||
+                        (i + j == n + 1 && i <= middle)
+                    )
                     {
                         Console.Write(" * ");
                     }
